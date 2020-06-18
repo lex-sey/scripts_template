5ca31e7ee46ebf5acd7adbf854e5640fbda0028d
lex-sey <67049478+lex-sey@users.noreply.github.com>
2020-06-18 15:49:24 +0300

    Âû÷èñëåíèå ðàçìåðà îïåðàòèâíîé ïàìÿòè ñ ïîìîùüþ óòèëèòû awk ëîêàëüíî è óäàëåííî ïî ssh

--- awk_ssh.sh
+++ awk_ssh.sh
@@ -4,7 +4,7 @@ ssh root@$IPSTANDBY '
 TOTALMEM=$(free -m | grep Mem: | cut -d " " -f 7-15 | sed "s/' '//g");
 echo 1 $TOTALMEM
 
-TOTALMEM2=$(free -m | awk 'NR==2{print$2}')
+TOTALMEM2=$(free -m | awk "NR==2{print\$2}")
 echo 2 $TOTALMEM2
 TOTALMEM3= free -m | awk "NR==2 {print \$2}"
 echo 3 $TOTALMEM3;
