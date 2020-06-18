ce5c8dd547f10e9b3f7149102c280960beb7b1f4
lex-sey <67049478+lex-sey@users.noreply.github.com>
2020-06-18 15:19:51 +0300

    Шаблон вывода размера оперативной памяти с помощью утилиты awk локально и удаленно

---
+++ awk_ssh.sh
@@ -0,0 +1,13 @@
+export IPSTANDBY=192.168.1.54
+
+ssh root@$IPSTANDBY '
+TOTALMEM=$(free -m | grep Mem: | cut -d " " -f 7-15 | sed "s/' '//g");
+echo 1 $TOTALMEM
+
+TOTALMEM2=$(free -m | awk 'NR==2{print$2}')
+echo 2 $TOTALMEM2
+TOTALMEM3= free -m | awk "NR==2 {print \$2}"
+echo 3 $TOTALMEM3;
+TOTALMEM4=$(free -m | awk NR\==\ 2\{print\ \$2\})
+echo 4 $TOTALMEM4
+'
